fc7ad7a10e11f0d0f714bbb9bf2887d112bc3487
Matheus Alcance <limariosprofissional@gmail.com>
2025-07-08 11:36:54 -0300

    atualiza configuração do Celery para incluir serialização e gerenciamento de resultados

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -7,3 +7,13 @@ celery_app = Celery(
     backend=settings.CELERY_RESULT_BACKEND
 )
 celery_app.autodiscover_tasks(['app.services'])
+
+celery_app.conf.update(
+    task_track_started=True,
+    task_ignore_result=False,
+    result_expires=3600,  # mantém resultado por 1 hora
+    result_backend=settings.CELERY_RESULT_BACKEND,
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"]
+)
