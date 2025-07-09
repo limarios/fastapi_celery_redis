1e0e2eca415ee2b2f6f70dc62d7cead1c62b833b
Matheus Alcance <limariosprofissional@gmail.com>
2025-07-09 09:07:06 -0300

    adiciona variáveis de configuração para chave secreta, algoritmo e URL do banco de dados

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,13 @@
+from typing import ClassVar
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     CELERY_BROKER_URL: str = "redis://redis:6379/0"
     CELERY_RESULT_BACKEND: str = "redis://redis:6379/0"
+
+    SECRET_KEY: str = "teste"
+    ALGORITHM: str = "HS256"
+    DATABASE_URL: ClassVar[str] = "mysql+pymysql://root:root@mysql:3306/automacoes"
+
 
 settings = Settings()
