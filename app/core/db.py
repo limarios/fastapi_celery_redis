c0eb2e79122cee783738e63042affcc95e1d04a3
Matheus Alcance <limariosprofissional@gmail.com>
2025-07-10 08:37:07 -0300

    Corrigindo arquivo de banco de dados

--- app/core/db.py
+++ app/core/db.py
@@ -1,6 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from app.core.config import settings
+from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# Dependency
+def get_db() -> Session: # type: ignore
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
