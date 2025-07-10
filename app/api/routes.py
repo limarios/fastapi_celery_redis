74548a9a01d6fc44d38f0d04aa96b6a7c4a9248a
Matheus Alcance <limariosprofissional@gmail.com>
2025-07-10 11:32:42 -0300

    ajustando rota

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from celery.result import AsyncResult
 from app.core.celery_app import celery_app
 from app.services.automacao import executar_automacao
-from app.core.db import SessionLocal, Session, get_db
+from app.core.db import get_db, Session
 from app.models.worker import Worker
 
 router = APIRouter()
@@ -14,18 +14,20 @@ class AutomacaoInput(BaseModel):
 @router.post("/executar")
 def start_automacao(payload: AutomacaoInput, db: Session = Depends(get_db)):
     task = executar_automacao.delay(payload.cliente_id)
-    db = SessionLocal()
-    try:
-        registro = Worker(
-            cliente_id=payload.cliente_id,
-            task_id=task.id,
-            status="AGENDADO"
-        )
-        db.add(registro)
-        db.commit()
-    finally:
-        db.close()
-    return {"status": "agendado", "task_id": task.id, "cliente_id": payload.cliente_id}
+
+    registro = Worker(
+        cliente_id=payload.cliente_id,
+        task_id=task.id,
+        status="AGENDADO"
+    )
+    db.add(registro)
+    db.commit()
+
+    return {
+        "status": "agendado",
+        "task_id": task.id,
+        "cliente_id": payload.cliente_id
+    }
 
 @router.get("/status/{task_id}")
 def get_status(task_id: str):
@@ -37,17 +39,13 @@ def get_status(task_id: str):
     }
 
 @router.get("/automacoes/{cliente_id}")
-def listar_automacoes(cliente_id: int):
-    db = SessionLocal()
-    try:
-        registros = db.query(Worker).filter_by(cliente_id=cliente_id).all()
-        return [
-            {
-                "task_id": r.task_id,
-                "status": r.status,
-                "data_inicio": r.data_inicio,
-                "data_fim": r.data_fim
-            } for r in registros
-        ]
-    finally:
-        db.close()
+def listar_automacoes(cliente_id: int, db: Session = Depends(get_db)):
+    registros = db.query(Worker).filter_by(cliente_id=cliente_id).all()
+    return [
+        {
+            "task_id": r.task_id,
+            "status": r.status,
+            "data_inicio": r.data_inicio,
+            "data_fim": r.data_fim
+        } for r in registros
+    ]
